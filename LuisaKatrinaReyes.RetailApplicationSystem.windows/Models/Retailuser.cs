12be3483194ef2ddddbc4458f457182e1e32562d
Luisa Katrina Reyes <lkpreyes@csmbataan.ph.education>
2020-10-01 13:42:02 +0800

    Project Files

---
+++ LuisaKatrinaReyes.RetailApplicationSystem.windows/Models/Retailuser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuisaKatrinaReyes.RetailApplicationSystem.windows.Models
+{
+    class Retailuser
+    {
+        public Guid? Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public decimal ContactNumber { get; set; }
+
+        public string EmailAddress { get; set; }
+
+    }
+}
